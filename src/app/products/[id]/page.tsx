a8fc0c1dc363385920b2d6fcc4e81a37a6b7141f
atfdeenk <atfdeenk@gmail.com>
2025-05-02 06:01:09 +0700

    Change loading overlay background color to solid black

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -86,7 +86,7 @@ export default function ProductDetail() {
 
   if (loading) {
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
         <LoadingOverlay message="Loading product details..." />
       </div>
     );
